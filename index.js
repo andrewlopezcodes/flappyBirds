ff0a3a89b6f0ec92919862ce96ce49a22137f89d
mrandrewlopez <andrewlopezcodes@gmail.com>
2018-07-27 23:47:41 -0400

    created canvas

--- index.js
+++ index.js
@@ -17,7 +17,20 @@
   pipes = {};
 
   function main(){
+    canvas = document.createElement('canvas');
 
+    width = window.innerWidth;
+    height = window.innerHeight;
+
+    if(width >= 500){
+      width = 320;
+      height = 480;
+    }
+    canvas.width = width;
+    canvas.height = height;
+    ctx = canvas.getContext("2d");
+
+    document.body.appendChild(canvas);
   }
 
   function run(){
