53d079a4f237a43016fbd8d6a0b6725372339e9b
mrandrewlopez <andrewlopezcodes@gmail.com>
2018-07-27 23:37:25 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+  var
+  canvas,
+  ctx,
+  width,
+  height,
+  frames = 0,
+  score = 0,
+  best = 0,
+
+  currentstate,
+  states = {
+    Splash: 0, Games: 1, Score: 2
+  }
+
+  bird = {},
+  pipes = {};
+
+  function main(){
+
+  }
+
+  function run(){
+
+  }
+
+  function update(){}
+  function render(){}
+
+  main();
